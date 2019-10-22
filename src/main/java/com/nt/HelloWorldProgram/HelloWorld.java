06b7e1a30c25984c20108cde4ab516f055eaffa1
jitu <jitendratripathy53@gmail.com>
2019-10-23 00:22:05 +0530

    intialize the fild value by constructor

--- src/main/java/com/nt/HelloWorldProgram/HelloWorld.java
+++ src/main/java/com/nt/HelloWorldProgram/HelloWorld.java
@@ -1,6 +1,10 @@
 package com.nt.HelloWorldProgram;
 
 public class HelloWorld {
-private int x;
-private String y;
+	private int x;
+	private String y;
+	public HelloWorld(int x,String y){
+		this.x=x;
+		this.y=y;
+	}
 }
